eb05310e0666609249bbaf5a3f6140d8df352eae
Facundo Molina <fmolina@dc.exa.unrc.edu.ar>
2017-09-16 18:20:27 -0300

    Initial commit

---
+++ word-clustering.py
@@ -0,0 +1,9 @@
+# Clustering
+import nltk
+
+sentence = """At eight o'clock on Thursday morning Arthur didn't feel very good."""
+
+tokens = nltk.word_tokenize(sentence)
+
+tagged = nltk.pos_tag(tokens)
+
